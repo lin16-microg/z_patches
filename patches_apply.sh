45615ee192fcefcf0e664e4c8b680f52c33e6fa4
MSe1969 <mse1969@posteo.de>
2019-03-15 12:04:45 +0100

    Add patch for build signing
    Change-Id: I09fa2cd906407a21f157386cac669ecb1c0258f2

--- patches_apply.sh
+++ patches_apply.sh
@@ -8,6 +8,13 @@ TOPDIR=$PWD
 cd build/make
 echo "Patching $PWD (user/host metadata)"
 patch -p1 < $THISDIR/patch_001_build.patch
+echo "-"
+cd $TOPDIR
+
+cd vendor/lineage
+echo "Patching $PWD (build signing method)"
+patch -p1 < $THISDIR/patch_002_vendor-lineage.patch
+echo "-"
 cd $TOPDIR
 
 cd $THISDIR
