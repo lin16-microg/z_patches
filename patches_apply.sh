87add3908ab288399785e2f4914b113c34288ab8
MSe1969 <mse1969@posteo.de>
2019-03-19 19:26:06 +0100

    microG-branch patch for SetupWizard
    An own fork of SuW for the small commit is like an overkill, so let's
    simply patch it, when the microG flavor is built.
    
    Change-Id: I6a6c9a616a2c3e5522c40dbc37a8f9109414e18d

--- patches_apply.sh
+++ patches_apply.sh
@@ -8,7 +8,15 @@ TOPDIR=$PWD
 cd build/make
 echo "Patching $PWD (user/host metadata)"
 patch -p1 < $THISDIR/patch_001_build.patch
+echo "-"
 cd $TOPDIR
+
+cd packages/apps/SetupWizard
+echo "Patching $PWD (Setup Wizard)"
+patch -p1 < $THISDIR/patch_002_SetupWizard.patch
+echo "-"
+cd $TOPDIR
+
 
 cd $THISDIR
 
