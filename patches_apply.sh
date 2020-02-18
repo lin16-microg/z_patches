4f18be21bce922b8916b7865b5f9483f07765933
MSe1969 <mse1969@posteo.de>
2020-02-18 23:01:07 +0100

    OTA Update patch
    Change-Id: Ifb8ef4fbdcd90530a4fa2c5c69095c999d33a1a9

--- patches_apply.sh
+++ patches_apply.sh
@@ -14,6 +14,7 @@ cd $TOPDIR
 cd vendor/lineage
 echo "Patching $PWD (build signing method)"
 patch -p1 < $THISDIR/patch_002_vendor-lineage.patch
+patch -p1 < $THISDIR/patch_003_vendor-lineage.patch
 echo "-"
 cd $TOPDIR
 
