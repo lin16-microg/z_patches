2c134cd301a0d8664f54682fd52fbe3770f7e9b6
MSe1969 <mse1969@posteo.de>
2020-03-25 10:03:45 +0100

    Remove build patch (build repo now own fork)
    Change-Id: I361b7703ce0e6ad25e2ac38beeaf6bcb68d26b65

--- patches_apply.sh
+++ patches_apply.sh
@@ -5,12 +5,6 @@ THISDIR=$PWD
 cd ..
 TOPDIR=$PWD
 
-cd build/make
-echo "Patching $PWD (user/host metadata)"
-patch -p1 < $THISDIR/patch_001_build.patch
-echo "-"
-cd $TOPDIR
-
 cd packages/apps/SetupWizard
 echo "Patching $PWD (Setup Wizard)"
 patch -p1 < $THISDIR/patch_002_SetupWizard.patch
--- patches_reverse.sh
+++ patches_reverse.sh
@@ -18,7 +18,6 @@ cd ..
 TOPDIR=$PWD
 
 
-clear_set build/make
 clear_set packages/apps/SetupWizard
 
 cd $THISDIR
