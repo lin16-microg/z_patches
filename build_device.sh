d7d464cf2098de62918b19090e3189ff893c0edc
MSe1969 <mse1969@posteo.de>
2020-10-15 21:01:06 +0200

    Build-script: Remove devices with 3.10 kernel
    Change-Id: I4a8eb220d96be6d8c510117c2313636cfd33a564

--- build_device.sh
+++ build_device.sh
@@ -3,7 +3,7 @@
 print_help() {
   echo "usage: build_device <device> test|sign [root]"
   echo "----------------------------------------------------------------------"
-  echo " <device> Device name (gts210ltexx|gts210vewifi|oneplus3|osprey)"
+  echo " <device> Device name (oneplus3)"
   echo "test - build with testkeys (insecure, but compatible)"
   echo "sign - create a signed build"
   echo "root - optional, if passed, build with root baked in"
@@ -16,7 +16,7 @@ print_device() {
 
 # Check parameters
 case "$1" in
-  gts210ltexx|gts210vewifi|oneplus3|osprey|x86)
+  oneplus3)
      print_device $1
     ;;
   *) print_help
