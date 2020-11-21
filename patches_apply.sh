881386b33871d00f4d33df99fe0c83459358f978
MSe1969 <mse1969@posteo.de>
2020-11-21 10:17:36 +0100

    Revert "Fix AOSP E-Mail Widget patch"
    This reverts commit e41e111ddc9f7a21371ead2b169460ffd37084c0.
    
    Patch has been merged upstream (LineageOS).

--- patches_apply.sh
+++ patches_apply.sh
@@ -35,12 +35,6 @@ patch -p1 < $THISDIR/patch_003_Camera2.patch
 echo "-"
 cd $TOPDIR
 
-cd packages/apps/Email
-echo "Patching $PWD (AOSP E-Mail Widget)"
-patch -p1 < $THISDIR/patch_010_Email.patch
-echo "-"
-cd $TOPDIR
-
 
 list_repos | while read STR; do
   DIR=$(echo $STR | cut -f1 -d:)
@@ -52,4 +46,8 @@ list_repos | while read STR; do
 done
 
 cd $THISDIR
+
+
+
+
 
