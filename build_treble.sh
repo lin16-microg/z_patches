94fd65aefb9138487eb122997e756c4aa06031b2
MSe <mse1969@posteo.de>
2023-08-15 23:26:14 +0200

    build-script: fix symlink recursions (and white spaces)
    Change-Id: I8787318f32ad01d2878d3917def91014d8ffdb92

--- build_treble.sh
+++ build_treble.sh
@@ -25,7 +25,7 @@ case "$1" in
      echo "root - if passed, include root"
      echo "zip  - if passed, create also a compressed ZIP file"
      exit
-    ;;   
+    ;;
 esac
 
 # Initiate environment
@@ -33,7 +33,7 @@ source build/envsetup.sh
 
 # CCache
 # ------
-# uncomment the below line to set own cache 
+# uncomment the below line to set own cache
 # directory (default is ~/.ccache)
 export USE_CCACHE=1
 #export CCACHE_DIR=~/android/.ccache
@@ -47,14 +47,14 @@ if [ "$TESTKEY" = false ] ; then
   export OWN_KEYS_DIR=~/.android-certs
   export RELEASE_TYPE=UNOFFICIAL-microG-signed
 
-  # We need symlinks to fake the existence of a testkey 
+  # We need symlinks to fake the existence of a testkey
   # for the selinux build process
   if [ ! -e $OWN_KEYS_DIR/testkey.pk8 ] ; then
-    ln -s $OWN_KEYS_DIR/releasekey.pk8 $OWN_KEYS_DIR/testkey.pk8
+    ln -s releasekey.pk8 $OWN_KEYS_DIR/testkey.pk8
     echo "Symlink testkey.pk8 created"
   fi
   if [ ! -e $OWN_KEYS_DIR/testkey.x509.pem ] ; then
-    ln -s $OWN_KEYS_DIR/releasekey.x509.pem $OWN_KEYS_DIR/testkey.x509.pem
+    ln -s releasekey.x509.pem $OWN_KEYS_DIR/testkey.x509.pem
     echo "Symlink testkey.x509.pem created"
   fi
 else
@@ -78,4 +78,3 @@ fi
 
 lunch treble_arm64_avN-userdebug
 make WITHOUT_CHECK_API=true systemimage && create_files
-
